4d2494361e8acd5e0beb457b98b4e896de093869
Alexcmyk <alex.kirts@gmail.com>
2021-05-31 09:18:35 -0600

    change one main image, window resize js

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,3 +1,4 @@
+const wd = document.defaultView;
 const menuBtn = document.querySelector('.mobile-button');
 const menuBtnArr = Array.from(menuBtn.children);
 const mobileNav = document.querySelector('#mobile-nav');
@@ -5,15 +6,28 @@ const mobileNav = document.querySelector('#mobile-nav');
 menuBtn.addEventListener('click', function () {
   if (mobileNav.classList.contains('open')) {
     mobileNav.classList.remove('open');
+    menuBtn.classList.remove('close');
     document.body.style.overflow = 'auto';
     menuBtnArr.forEach((span) => {
       span.style.backgroundColor = '#000';
     });
   } else {
     mobileNav.classList.add('open');
+    menuBtn.classList.add('close');
     document.body.style.overflow = 'hidden';
     menuBtnArr.forEach((span) => {
       span.style.backgroundColor = '#fff';
+    });
+  }
+});
+
+wd.addEventListener('resize', function () {
+  if (window.innerWidth > 700) {
+    mobileNav.classList.remove('open');
+    menuBtn.classList.remove('close');
+    document.body.style.overflow = 'auto';
+    menuBtnArr.forEach((span) => {
+      span.style.backgroundColor = '#000';
     });
   }
 });
